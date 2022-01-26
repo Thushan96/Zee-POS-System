ee5bd60094d5e47d69d94388c7088fd3624fd0ba
Thushan96 <thushanmadhulakshitha@gmail.com>
2022-01-26 15:04:40 +0530

    turned in to single page

---
+++ Assets/js/script.js
@@ -0,0 +1,27 @@
+document.getElementById("home-nav").addEventListener("click",function (){
+    document.getElementById("sec1").style.display="block";
+    document.getElementById("sec2").style.display="none";
+    document.getElementById("sec3").style.display="none";
+    document.getElementById("sec4").style.display="none";
+});
+
+document.getElementById("customer-nav").addEventListener("click",function (){
+    document.getElementById("sec2").style.display="block";
+    document.getElementById("sec1").style.display="none";
+    document.getElementById("sec3").style.display="none";
+    document.getElementById("sec4").style.display="none";
+});
+
+document.getElementById("Item-nav").addEventListener("click",function (){
+    document.getElementById("sec2").style.display="none";
+    document.getElementById("sec1").style.display="none";
+    document.getElementById("sec3").style.display="block";
+    document.getElementById("sec4").style.display="none";
+});
+
+document.getElementById("ODetails-nav").addEventListener("click",function (){
+    document.getElementById("sec2").style.display="none";
+    document.getElementById("sec1").style.display="none";
+    document.getElementById("sec3").style.display="none";
+    document.getElementById("sec4").style.display="block";
+});
