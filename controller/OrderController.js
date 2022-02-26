5389270f8843edd86b6a1dca638c35b5e205e6ab
Thushan96 <thushanmadhulakshitha@gmail.com>
2022-02-26 15:59:01 +0530

    updated content

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -51,7 +51,102 @@ $(document).ready(function () {
 $("#order-date").val(d.getFullYear()+'/'+(d.getMonth()+1)+'/'+d.getDate());
 });
 
+$("#qty-order").on('keyup', function (){
+    var resp=OrderformValid();
+    alert(resp);
+    if (resp){
+        $("#add-order").attr('disabled',false);
+    }else{
+        $("#add-order").attr('disabled',true);
+    }
+});
+
+
 $("#add-order").click(function (){
     var orderQTY=$("#qty-order").val();
-
 });
+
+
+// VALIDATIONS
+
+const cusQtyRegEx = /^[0-9]{1,3}$/;
+
+
+
+function OrderformValid() {
+    var cusID=$("#customer-name-order").val();
+    if (cusNameRegEx.test(cusID)) {
+        var itemName=$("#item-name-order").val();
+        if(itemNameRegEx.test(itemName)){
+            var itemPrice=$("#unit-price-order").val();
+            if (itemPriceRegEx.test(itemPrice)){
+                var QtyOnHand=$("#qtyOnHand-order").val();
+                if (itemQtyRegEx.test(QtyOnHand)){
+                    var reqQty=$("#qty-order").val();
+                    var status=cusQtyRegEx.test(reqQty);
+                    if (status){
+                        $("#qty-order").css('border', '2px solid blue');
+                        $("#lblReqQty").text("");
+                        return true;
+                    }else{
+                        $("#qty-order").css('border', '2px solid red');
+                        $("#lblReqQty").text("Order Quantity is a required field : maximum 3 digits");
+                        return false;
+                    }
+
+                }else{
+                    return false;
+                }
+
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
+
+}
+
+function formValid() {
+    var cusID = $("#customer-id").val();
+    $("#customer-id").css('border', '2px solid green');
+    $("#lblcusid").text("");
+    if (cusIDRegEx.test(cusID)) {
+        var cusName = $("#customer-name").val();
+        if (cusNameRegEx.test(cusName)) {
+            $("#customer-name").css('border', '2px solid green');
+            $("#lblcusname").text("");
+            var cusAddress = $("#customer-address").val();
+            if (cusAddressRegEx.test(cusAddress)) {
+                var cusMobile = $("#customer-mobile").val();
+                var resp = cusMobileRegEx.test(cusMobile);
+                $("#customer-address").css('border', '2px solid green');
+                $("#lblcusaddress").text("");
+                if (resp) {
+                    $("#customer-mobile").css('border', '2px solid green');
+                    $("#lblcusCno").text("");
+                    return true;
+                } else {
+                    $("#customer-mobile").css('border', '2px solid red');
+                    $("#lblcusCno").text("Customer Mobile No is a required field : 10 digits");
+                    return false;
+                }
+            } else {
+                $("#customer-address").css('border', '2px solid red');
+                $("#lblcusaddress").text("Customer Address  is a required field : Minimum 7");
+                return false;
+            }
+        } else {
+            $("#customer-name").css('border', '2px solid red');
+            $("#lblcusname").text("Customer Name is a required field : Minimum 5, Max 20, Spaces Allowed");
+            return false;
+        }
+    } else {
+        $("#customer-id").css('border', '2px solid red');
+        $("#lblcusid").text("Customer ID is a required field : Pattern C00-000");
+        return false;
+    }
+}
